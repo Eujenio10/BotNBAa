dddfb1cac688b757871a8215dc8572d96c36b9e1
Eujenio10 <39347354+Eujenio10@users.noreply.github.com>
2025-05-08 11:06:26 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--disable-images")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36')
+
+driver = webdriver.Chrome(options=chrome_options)
